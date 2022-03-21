d19fa2780694b0b06807378964da36d068a30114
miaoermua <miaoermua@gmail.com>
2022-03-21 23:58:09 +0800

    Site updated: 2022-03-21 23:58:07

--- js/comments/waline.js
+++ js/comments/waline.js
@@ -1,1 +1,1 @@
-import{d as i}from"../chunk-YZ7E3ATC.js";import"../chunk-TTX6TVTO.js";i(window.CONFIG.waline.cdn,()=>{new window.Waline(window.CONFIG.waline.config)},window.Waline);
+import{d as n}from"../chunk-YZ7E3ATC.js";import"../chunk-TTX6TVTO.js";function e(){n(window.CONFIG.waline.cdn,()=>{new window.Waline(window.CONFIG.waline.config)},window.Waline)}document.addEventListener("DOMContentLoaded",e);document.addEventListener("pjax:success",e);
